9ad3d8b4cc64c806737e906c593085aa8fea6b17
PanGDX <prtanprasertkul@gmail.com>
2025-03-30 22:20:09 +0800

    updating info

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
     "Svelte",
   ];
 
-  const backendSkills = ["Node.js", "Python", "AWS", "MongoDB", "GraphQL"];
+  const Skills = ["Python", "Javascript", "C++"];
 
   return (
     <section
@@ -25,31 +25,14 @@ export const About = () => {
 
           <div className="rounded-xl p-8 border-white/10 border hover:-translate-y-1 transition-all">
             <p className="text-gray-300 mb-6">
-              Passionate developer with expertise in building scalable web
-              applications and creating innovative solutions.
+              Passionate developer with expertise in several fields, building applications and creating innovative solutions.
             </p>
 
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Frontend</h3>
+                <h3 className="text-xl font-bold mb-4">Software Skills</h3>
                 <div className="flex flex-wrap gap-2">
-                  {frontendSkills.map((tech, key) => (
-                    <span
-                      key={key}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
-                    "
-                    >
-                      {tech}
-                    </span>
-                  ))}
-                </div>
-              </div>
-
-              <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
-                <h3 className="text-xl font-bold mb-4"> Backend</h3>
-                <div className="flex flex-wrap gap-2">
-                  {backendSkills.map((tech, key) => (
+                  {Skills.map((tech, key) => (
                     <span
                       key={key}
                       className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -68,13 +51,12 @@ export const About = () => {
             <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
               <h3 className="text-xl font-bold mb-4"> 🏫 Education </h3>
               <ul className="list-disc list-inside text-gray-300 space-y-2">
+                <strong>National University of Singapore (2024-2028)</strong>
                 <li>
-                  <strong> B.S. in Computer Science </strong> - XYZ University
-                  (2016-2020)
+                  B.S. in Computer Engineering
                 </li>
                 <li>
-                  Relevant Coursework: Data Structures, Web Development, Cloud
-                  Computing...
+                  NUS College - Liberal Arts Education
                 </li>
               </ul>
             </div>
@@ -84,7 +66,7 @@ export const About = () => {
                 <div>
                   <h4 className="font-semibold">
                     {" "}
-                    Software Engineer at ABC Corp (2020 - Present){" "}
+                    Lotus's (Apr 2024 - May 2024){" "}
                   </h4>
                   <p>
                     Developed and maintained microservices for cloud-based
@@ -95,11 +77,68 @@ export const About = () => {
                 <div>
                   <h4 className="font-semibold">
                     {" "}
-                    Intern at DEF Startups (2019){" "}
+                    Electronic Transactions Development Agency, Thailand (ETDA) (Jan 2024 - Mar 2024){" "}
                   </h4>
                   <p>
                     Assisted in building front-end components and integration
                     REST APIs
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 💼 Achievements </h3>
+              <div className="space-y-4 text-gray-300">
+                <div>
+                  <h4 className="font-semibold">
+                    IDEATE 2024: Future Home Edition{" "}
+                  </h4>
+                  <p>
+                    Most Sustainable Solution
+                  </p>
+                </div>
+                <div>
+                  <h4 className="font-semibold">
+                    ASEAN Scholarship{" "}
+                  </h4>
+                </div>
+                <div>
+                  <h4 className="font-semibold">
+                    National Olympiad for Informatics, Singapore (2023){" "}
+                  </h4>
+                  <p>
+                    Silver Medalist
+                  </p>
+                </div>
+                <div>
+                  <h4 className="font-semibold">
+                    National Olympiad for Informatics, Singapore (2021){" "}
+                  </h4>
+                  <p>
+                    Silver Medalist
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border-white/10 border hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4"> 💼 My Hobbies </h3>
+              <div className="space-y-4 text-gray-300">
+                <div>
+                  <h4 className="font-semibold">
+                    Muay Thai
+                  </h4>
+                  <p>
+                    Developed and maintained microservices for cloud-based
+                    applications.
+                  </p>
+                </div>
+
+                <div>
+                  <h4 className="font-semibold">
+                    Reading
+                  </h4>
+                  <p>
+                    
                   </p>
                 </div>
               </div>
